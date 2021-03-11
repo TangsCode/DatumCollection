f998770240d34b3d4629026053209f265e825197
tangscode <tangscode@163.com>
2021-03-11 17:33:50 +0800

    new feature: cache implementation in memory cache

---
+++ DatumCollection.Cache/ICache.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatumCollection.Cache
+{
+    /// <summary>
+    /// 缓存接口
+    /// </summary>
+    interface ICache
+    {
+        Task<T> GetOrCreate<T>(object key, Func<Task<T>> createItem);
+    }
+}
